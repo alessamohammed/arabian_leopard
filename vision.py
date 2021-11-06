f34dd7a5c5ced0a4dd057784c799ac04d734be6f
Mohammed Alessa <mohammed_13_a@hotmail.com>
2021-11-06 04:27:50 +0300

    added different types of possiable responses to picture

--- vision.py
+++ vision.py
@@ -2,28 +2,68 @@ import io
 import os
 from google.cloud import vision
 
-def animal_type():
-    # Imports the Google Cloud client library
+# Imports the Google Cloud client library
 
 
-    # Instantiates a client
-    client = vision.ImageAnnotatorClient()
+# Instantiates a client
+client = vision.ImageAnnotatorClient()
 
-    # The name of the image file to annotate
-    file_name = os.path.abspath('arabian.jpg')
+# The name of the image file to annotate
+file_name = os.path.abspath('arabian4.jpg')
 
-    # Loads the image into memory
-    with io.open(file_name, 'rb') as image_file:
-        content = image_file.read()
+# Loads the image into memory
+with io.open(file_name, 'rb') as image_file:
+    content = image_file.read()
 
-    image = vision.Image(content=content)
+image = vision.Image(content=content)
 
-    # Performs label detection on the image file
-    response = client.label_detection(image=image)
-    labels = response.label_annotations
+# Performs label detection on the image file
+response = client.label_detection(image=image)
+labels = response.label_annotations
 
-    print('Labels:')
-    for label in labels:
-        print(label.description)
+print (response)
 
-    return label.description
+Leopard_score=0
+BigCats_score=0
+Cheetah_score=0
+Jaguar_score=0
+
+print('Labels:')
+
+for label in labels:  
+    if label.description=="Leopard":
+        Leopard_score = label.score
+        Leopard_description = label.description
+        
+    if label.description=="Big cats":
+        BigCats_score = label.score
+        BigCats_description = label.description
+        
+    if label.description=="Cheetah":
+        Cheetah_score = label.score
+        Cheetah_description = label.description
+       
+    if label.description=="Jaguar":
+        Jaguar_score = label.score
+        Jaguar_description = label.description
+        
+
+if Leopard_score !=0:
+    print(Leopard_description)
+    print(Leopard_score)
+
+
+elif Cheetah_score !=0:
+    print(Cheetah_description)
+    print(Cheetah_score)
+
+elif BigCats_score !=0:
+    print(BigCats_description)
+    print(BigCats_score)
+
+elif Jaguar_score !=0:
+    print(Jaguar_score)
+    print(Jaguar_description)
+
+else:
+    print("The leopard was not found")
